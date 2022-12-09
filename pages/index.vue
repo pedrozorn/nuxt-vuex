1da5d2bbf4836014fd756f753a2441fce52fb87c
pedrozorn <itt.pedrodev53@gmail.com>
2022-12-09 22:21:12 +0900

    vuex

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    <div>
+      <!-- <p>{{ $store.state.message}}</p> -->
+      <p>{{ $store.state.hello.message }}</p>
+      <!-- <button v-on:click="$store.commit('updateMessage','')">Update</button> -->
+      <!-- <button v-on:click="$store.commit('updateMessage', 'commit with pay load')">Update</button> -->
+      <!-- <button v-on:click="$store.dispatch('updateMessageAction')">dispatch</button> -->
+      <button v-on:click="$store.dispatch('hello/updateMessageAction','hikisuuwatasi')">dispatch</button>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+}
+</script>
